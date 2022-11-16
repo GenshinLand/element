189b2c1d219df629cad48ea533e9c82af4be4568
李铭昕 <715557344@qq.com>
2022-11-16 14:35:52 +0800

    Format code

--- src/Cryo.php
+++ src/Cryo.php
@@ -1,11 +1,19 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 namespace Genshin\Element;
 
 /**
- * 冰元素
+ * 冰元素.
  */
 class Cryo
 {
-
 }
--- src/Pyro.php
+++ src/Pyro.php
@@ -1,11 +1,19 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 namespace Genshin\Element;
 
 /**
- * 火元素
+ * 火元素.
  */
 class Pyro
 {
-
 }
